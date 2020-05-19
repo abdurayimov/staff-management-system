63d35f6f58bd8dbea72b035e0e2dad93306bfa0b
Lenmar A <abdurayimov@mail.ru>
2020-05-19 13:02:17 +0300

    'changes'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,8 +54,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
+                            <li><a href="{{ route('login') }}">Вход</a></li>
+                            <li><a href="{{ route('register') }}">Регистрация</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -65,14 +65,14 @@
                                 <ul class="dropdown-menu" role="menu">
 
                                     <li>
-                                        <a href="{{ route('edit-staff', Auth::user()) }}" class="btn btn-primary btn-md"><i class="fa fa-plus"></i> Edit Profile</a>
+                                        <a href="{{ route('edit-staff', Auth::user()) }}" class="btn btn-primary btn-md"><i class="fa fa-plus"></i> Редактировать профиль</a>
                                     </li>
 
                                     <li>
                                         <a href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Выход
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
